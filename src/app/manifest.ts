8cdada2f09e7997884de78caecdb570b855ffc5f
keadevs <51330099+keadevs@users.noreply.github.com>
2026-09-11 21:19:14 +0530

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon.svg',
         sizes: '192x192',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/icon.svg',
